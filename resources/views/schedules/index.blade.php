e6504a3af8287840063648e0e1c80b17b0755b53
AndoKeiko <gajumaro.no.ki@gmail.com>
2024-10-05 20:05:01 +0900

    csvボタン追加3

--- resources/views/schedules/index.blade.php
+++ resources/views/schedules/index.blade.php
@@ -37,7 +37,7 @@
         </button>
         <div id="calendar" class="mt-8"></div>
         <ul class="flex flex-nowrap flex-row justify-start items-center mt-4">
-          <li> <a id="saveAllTasksBtn" class="px-4 py-2 bg-green-500 text-white rounded-md hover:bg-green-600 focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-opacity-50">
+          <li> <a id="saveAllTasksBtn" class="px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50">
               全てのタスク・通知設定を保存
             </a></li>
           <li>
