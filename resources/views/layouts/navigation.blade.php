4afb8d8b5ee132ff510d27e6333a784df1dfd827
AndoKeiko <gajumaro.no.ki@gmail.com>
2024-10-05 15:50:59 +0900

    微調整

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -14,7 +14,7 @@
         </div>
 
         <!-- Navigation Links -->
-        <div class="hidden space-x-6 sm:-my-px sm:ms-10 sm:flex">
+        <div class="hidden space-x-6 sm:-my-px sm:ms-6 sm:flex">
           <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
             {{ __('ゆめがかなうかもしれないアプリ') }}
           </x-nav-link>
--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -4,18 +4,19 @@
 <div class="py-12">
   <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
     <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-      <div class="p-6 bg-white border-b border-gray-200">
-        <h2 class="text-2xl font-semibold mb-6">{{ $goal->name }}タスク一覧</h2>
+      <div class="p-6 bg-white">
+        <h2 class="text-2xl font-semibold">{{ $goal->name }}タスク一覧</h2>
       </div>
       <div class="my-2 flex sm:flex-row flex-col p-4">
         <div class="relative">
-          <span class="absolute inset-y-0 left-0 flex items-center pl-2">
+          <!-- <span class="absolute inset-y-0 left-0 flex items-center pl-2">
             <svg viewBox="0 0 24 24" class="h-4 w-4 fill-current text-gray-500">
               <path d="M10 4a6 6 0 100 12 6 6 0 000-12zm-8 6a8 8 0 1114.32 4.906l5.387 5.387a1 1 0 01-1.414 1.414l-5.387-5.387A8 8 0 012 10z"></path>
             </svg>
-          </span>
-          <input placeholder="タスクを検索"
-            class="appearance-none rounded-md border border-gray-300 pl-8 pr-6 py-2 w-full bg-white text-sm placeholder-gray-400 text-gray-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent" />
+          </span> -->
+          <p class="px-4">ドラックで順番が入れ替わります</p>
+          <!-- <input placeholder="タスクを検索"
+            class="appearance-none rounded-md border border-gray-300 pl-8 pr-6 py-2 w-full bg-white text-sm placeholder-gray-400 text-gray-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent" /> -->
         </div>
       </div>
       <div class="px-4 sm:px-8 py-4 overflow-x-auto">
@@ -81,12 +82,14 @@
         </div>
       </div>
     </div>
-    <div class="mt-8">
-      <a href="{{ route('schedules.index', $goal) }}" class="text-blue-600 hover:text-blue-800">カレンダーに書き出す</a>
-    </div>
-    <div class="mt-8">
-      <a href="{{ route('goals.index') }}" class="text-blue-600 hover:text-blue-800">目標ページに戻る</a>
-    </div>
+    <ul class="flex flex-nowrap flex-row justify-start items-center mt-4">
+          <li>
+          <a href="{{ route('schedules.index', $goal) }}" class="px-4 py-2 ml-2 bg-green-500 text-white rounded-md hover:bg-green-600 focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-opacity-50">カレンダーに書き出す</a>
+          </li>
+          <li>
+          <a href="{{ route('goals.index') }}" class="px-4 py-2 ml-2 bg-green-500 text-white rounded-md hover:bg-green-600 focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-opacity-50">目標ページに戻る</a>
+          </li>
+        </ul>
   </div>
 </div>
 @push('scripts')
